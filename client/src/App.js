538302e627458e6089f92acd4b29bf03fd3db19e
Gson44 <geric99997@gmail.com>
2019-07-14 15:47:53 -0400

    Open box starting page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+
+import './App.css';
 
 class App extends React.Component {
-  state = { serverMessage: '' }
-
-  componentDidMount(){
-    fetch('/api/demo')
-      .then(response => response.json())
-      .then(data => this.setState({ serverMessage: data.message }))
+  
+  searchResult = () =>{
+   // var results = document.getElementById('searchbar').value
+    fetch('https://api.bestbuy.com/beta/products/openBox&pageSize=20.json?apiKey=BKMqrrbJYO6728izPqs1mZuS')
+    .then(Response => Response.json())
+    .then(data => console.log(data.results))
   }
-
-  render(){
-    return (
-      <div id="demo">
-        <h1>Hello from client/src/App.js</h1>
-        <h1>{this.state.serverMessage}</h1>
+  
+  render() {
+  return (
+    <div className="App">
+      <h1 id="title">OpenBox Catalog</h1>
+      
+      <div class="submitForm"> 
+        <form>
+          <input type="text" id="searchbar" size="70" placeholder="Search Open Box Item"></input>
+        </form>
+        <button onClick={this.searchResult}>Submit</button>
       </div>
-    )
-  }
+     
+     
+    </div>
+  );
 }
+}
+export default App;
 
-export default App
