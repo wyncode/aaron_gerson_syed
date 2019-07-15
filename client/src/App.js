cc4f5900399d52e8f7620f97384a3a8dab42602a
Gson44 <geric99997@gmail.com>
2019-07-15 13:51:28 -0400

    Front end stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Search from './Search'
 
 class App extends React.Component {
   
-  searchResult = () =>{
-   // var results = document.getElementById('searchbar').value
-    fetch('https://api.bestbuy.com/beta/products/openBox&pageSize=20.json?apiKey=BKMqrrbJYO6728izPqs1mZuS')
-    .then(Response => Response.json())
-    .then(data => console.log(data.results))
-  }
+  
   
   render() {
   return (
     <div className="App">
-      <h1 id="title">OpenBox Catalog</h1>
-      
-      <div class="submitForm"> 
-        <form>
-          <input type="text" id="searchbar" size="70" placeholder="Search Open Box Item"></input>
-        </form>
-        <button onClick={this.searchResult}>Submit</button>
-      </div>
-     
-     
+      <Search />
     </div>
   );
 }
