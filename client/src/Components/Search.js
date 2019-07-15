4aa4069b11b583751f0dae02982c63a7874c3ef2
Aaron Holland <aaronholla@gmail.com>
2019-07-15 15:52:21 -0400

    Cleaned up Search Component

--- client/src/Components/Search.js
+++ client/src/Components/Search.js
@@ -1,30 +1,30 @@
-import React from 'react';
+import React from 'react'
 
- import styles from './Search.module.css';
+import styles from './Search.module.css'
 
- class Search extends React.Component {
-    searchResult = () =>{
-        // var results = document.getElementById('searchbar').value
-         fetch(`/products/search/${this.state.searchInput}`)
-         .then(Response => Response.json())
-         .then(data => console.log(data.results))
-       }
+class Search extends React.Component {
+  state = {
+    searchInput: ""
+  }
+  
+  searchResult = () => {
+    fetch(`/products/search/${this.state.searchInput}`)
+      .then(Response => Response.json())
+      .then(data => console.log(data.results))
+  }
 
-        state = {searchInput: ""}
-
-   render() {
-  return (
-    <div className={styles.search}>
-      <h1 id={styles.title}>OpenBox Catalog</h1>
-
-       <div id={styles.submitForm}> 
-        <form>
-          <input value = {this.state.searchinput} type="text" id={styles.searchBox} size="70" placeholder="Search Open Box Item"></input>
+  render() {
+    return (
+      <div className={styles.search}>
+        <h1 id={styles.title}>OpenBox Catalog</h1>
+        
+        <form id={styles.submitForm} onSubmit={this.searchResult}>
+          <input value={this.state.searchInput} type="text" id={styles.searchBox} size="70" placeholder="Search Open Box Item"></input>
+          <button type="submit">Submit</button>
         </form>
-        <button onClick={this.searchResult}>Submit</button>
       </div>
-    </div>
-  );
+    )
+  }
 }
-}
-export default Search;
+
+export default Search
