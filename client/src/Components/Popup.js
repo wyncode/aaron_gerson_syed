de56fce83dc7d6fe3a0f9d77303b7f8f40a8cc86
sidramen <sidlovessoup@gmail.com>
2019-07-16 11:22:36 -0400

    deleted the comment

--- client/src/Components/Popup.js
+++ client/src/Components/Popup.js
@@ -4,30 +4,7 @@ import styles from './popup.module.css';
 
 class Popup extends React.Component {
     state = {showing: false}
-   /*
-    // Get the modal
-    var modal = document.getElementById("myModal");
-    
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
-    
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }*/
-
-      //When the user clicks the button, open the modal 
+   
     openModal = e => {
       this.setState({showing: this.state.showing ? false : true})
     }
