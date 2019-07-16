0ba60e963768029c483424238e9cb6d9d427e7fe
Aaron Holland <aaronholla@gmail.com>
2019-07-16 19:26:06 -0400

    added results and passing results to resultsDisplay also updates as you type

--- client/src/Pages/Results.js
+++ client/src/Pages/Results.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import axios from 'axios'
+import { createBrowserHistory } from 'history'
+import Search from '../Components/Search'
+import ResultsComponent from '../Components/ResultsComponent'
 
-const Results = () => {
-  return(
-    <h1>This is the results page</h1>
-  )
+
+class Results extends React.Component {
+  params = new URLSearchParams(this.props.location.search)
+  state = {
+    results: []
+  }
+
+  updateSearchInput = (query) => {
+    const history = createBrowserHistory()
+    history.replace({
+      pathname: '/search',
+      search: `?query=${query}`
+    })
+    this.getNewResults(query)
+  }
+
+  getNewResults = (query) => {
+    axios(`/products/search/${query}`)
+    .then(response => this.setState({results: response.data}))
+  }
+
+  componentDidMount() {
+    this.getNewResults(this.params.get("query"))
+  }
+
+  render() {
+    const {results} = this.state
+    return(
+      <>
+        <Search onResults updateSearchInput={this.updateSearchInput}/>
+        <ResultsComponent results={results} />
+      </>
+    )
+  }
 }
 
 export default Results
