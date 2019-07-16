5de2f2a4979e21dcf922b948c8e8dd28678f5c70
Gson44 <geric99997@gmail.com>
2019-07-16 15:53:25 -0400

    Add results display and results component

---
+++ client/src/Components/ResultsComponent.js
@@ -0,0 +1,14 @@
+const ResultsComponent = (props) =>  {
+       
+    const items = this.props.map(item =>{
+      <li>
+          <h2>{this.props.title}</h2>
+          <img>{item.images.standard}</img>
+          <p>OpenBox Price: {item.prices.current}</p>
+          <p>Original Price: {item.prices.regular}</p>
+      </li>
+    })
+    return items
+}
+
+export default ResultsComponent
--- client/src/Components/ResultsDisplay.js
+++ client/src/Components/ResultsDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import resultComponent from 'resultComponent'
+import ResultsComponent from 'ResultsComponent'
 import styles from './Search.module.css'
 
 class Results extends React.Component {
@@ -8,7 +8,7 @@ class Results extends React.Component {
 
   render() {
     const items = this.props.results.map(item => (
-      <ResultComponent title={item.names.title} imgSrc={item.images.standard} currentPrice={item.offer[0].prices.current} originalPrice={item.offer[0].prices.regular} />
+      <ResultsComponent title={item.names.title} imgSrc={item.images.standard} currentPrice={item.offer[0].prices.current} originalPrice={item.offer[0].prices.regular} />
      ))
 
     return (
