0ef54c73bf6e27e56e8e281170e8ed5e5300bded
Aaron Holland <aaronholla@gmail.com>
2019-07-17 15:25:44 -0400

    ResultList merge update

--- client/src/Components/ResultsList.js
+++ client/src/Components/ResultsList.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ResultsListItem from './ResultsListItem'
 import styles from './ResultsList.module.css'
 
-<<<<<<< HEAD
 const ResultsList = (props) => {
   return (
     <ul>
@@ -23,27 +22,6 @@ const ResultsList = (props) => {
       }
     </ul>
   )
-=======
-class ResultsList extends React.Component {
-  render() {
-    return (
-      <ul className={styles.results}>
-        {
-          this.props.results.map(item => (
-            <ResultsListItem
-              key={item.sku}
-              sku={item.sku}
-              title={item.names.title} 
-              imgSrc={item.images.standard} 
-              currentPrice={item.offers[0].prices.current} 
-              originalPrice={item.offers[0].prices.regular} 
-            />
-          ))
-        }
-      </ul>
-    )
-  }
->>>>>>> enhancement-26-gerson-style-results-page
 }
 
 export default ResultsList
