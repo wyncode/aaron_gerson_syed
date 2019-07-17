963ea62e2442ea259a01d15b92db8244c1d1cf55
Aaron Holland <aaronholla@gmail.com>
2019-07-16 20:12:35 -0400

    Fixed Results Display and Component

--- client/src/Components/ResultsComponent.js
+++ client/src/Components/ResultsComponent.js
@@ -1,14 +1,16 @@
+import React from 'react'
+
 const ResultsComponent = (props) =>  {
-       
-    const items = this.props.map(item =>{
-      <li>
-          <h2>{this.props.title}</h2>
-          <img>{item.images.standard}</img>
-          <p>OpenBox Price: {item.prices.current}</p>
-          <p>Original Price: {item.prices.regular}</p>
-      </li>
-    })
-    return items
+    return (
+      props.results.map(item => (
+        <li>
+            <h2>{item.names.title}</h2>
+            <img src={item.images.standard} alt={`${item.sku}`}/>
+            <p>OpenBox Price: {item.prices.current}</p>
+            <p>Original Price: {item.prices.regular}</p>
+        </li>
+      ))
+    )
 }
 
 export default ResultsComponent
--- client/src/Components/ResultsDisplay.js
+++ client/src/Components/ResultsDisplay.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import axios from 'axios'
 import ResultsComponent from 'ResultsComponent'
-import styles from './Search.module.css'
 
 class Results extends React.Component {
-  
-
   render() {
-    const items = this.props.results.map(item => (
-      <ResultsComponent title={item.names.title} imgSrc={item.images.standard} currentPrice={item.offer[0].prices.current} originalPrice={item.offer[0].prices.regular} />
-     ))
-
     return (
       <ul>
-        {items}
+        {
+          this.props.results.map(item => (
+            <ResultsComponent 
+              key={item.sku}
+              title={item.names.title} 
+              imgSrc={item.images.standard} 
+              currentPrice={item.offer[0].prices.current} 
+              originalPrice={item.offer[0].prices.regular} 
+            />
+          ))
+        }
       </ul>
     )
-
   }
 }
 
