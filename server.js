e508e33ce45e69512a65757cdc0deafb7e177c40
Aaron Holland <aaronholla@gmail.com>
2019-07-17 12:28:01 -0400

    added product detiail endpoint and added error catches

--- server.js
+++ server.js
@@ -8,10 +8,30 @@ const path = require("path")
 const app = express()
 
 app.get('/products/search/:query', async (request, response) => {
-  const {data} = await axios.get(`https://api.bestbuy.com/v1/products((search=${request.params.query})&manufacturer=Apple)?apiKey=${process.env.BESTBUY_API_KEY}&sort=sku.asc&show=sku&pageSize=20&format=json`)
-  const skuList = data.products.map(product => product.sku).join(",")
-  const openBox = await axios.get(`https://api.bestbuy.com/beta/products/openBox(sku%20in(${skuList}))?apiKey=${process.env.BESTBUY_API_KEY}&pageSize=20`)
-  response.send(openBox.data.results)
+  try {
+    const {data} = await axios.get(`https://api.bestbuy.com/v1/products((search=${request.params.query})&manufacturer=Apple)?apiKey=${process.env.BESTBUY_API_KEY}&sort=sku.asc&show=sku&pageSize=20&format=json`)
+    console.log(data)
+    if(data && data.products && data.products.length > 0) {
+      const skuList = data.products.map(product => product.sku).join(",")
+      const openBox = await axios.get(`https://api.bestbuy.com/beta/products/openBox(sku%20in(${skuList}))?apiKey=${process.env.BESTBUY_API_KEY}&pageSize=20`)  
+      response.send(openBox.data.results)  
+    } else {
+      response.send([])
+    }
+  } catch (e) {
+    console.log(e)
+    response.send([])
+  }
+})
+
+app.get('/products/:sku', async (request, response) => {
+  try {
+    const {data} = await axios.get(`https://api.bestbuy.com/beta/products/${request.params.sku}/openBox?apiKey=${process.env.BESTBUY_API_KEY}`)
+    response.send(data.results[0] || {})  
+  } catch (e) {
+    console.log(e)
+    response.send({})
+  }
 })
 
 if (process.env.NODE_ENV === 'production') {
